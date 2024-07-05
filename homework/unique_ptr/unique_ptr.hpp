7359ae96ff597a153782d84924a6686eda8bed2d
KrzucH <krzynowak93@gmail.com>
2024-07-05 15:28:24 +0200

    Small changes

--- homework/unique_ptr/unique_ptr.hpp
+++ homework/unique_ptr/unique_ptr.hpp
@@ -1,32 +1,33 @@
 #pragma once
 
-template <typename Type>
+template<typename Type>
 class uniq_ptr {
 public:
-    uniq_ptr() {
-        ptr_ = nullptr;
+    uniq_ptr() { 
+        ptr_ =  nullptr; 
     }
 
-    uniq_ptr(Type* uniq)
-        : ptr_(uniq) {}
+    uniq_ptr(Type* uniq) : ptr_(uniq)
+    {}
 
-    uniq_ptr(const uniq_ptr&) = delete;
+    uniq_ptr(const uniq_ptr&) = delete;        
+   
 
-    uniq_ptr(uniq_ptr&& uniq) {
-        ptr_ = uniq.ptr_;
-        uniq.ptr_ = nullptr;
+    uniq_ptr(uniq_ptr&& uniq)  {
+            ptr_ = uniq.ptr_;
+            uniq.ptr_ = nullptr;
     }
 
     uniq_ptr& operator=(const uniq_ptr&) = delete;
 
     uniq_ptr& operator=(const uniq_ptr&& uniq) {
-        if (uniq.ptr_ != nullptr) {
+        if(uniq.ptr_ != nullptr) {
             delete ptr_;
             ptr_ = uniq.ptr_;
             uniq.ptr_ = nullptr;
         }
         return *this;
-    }
+    }   
 
     ~uniq_ptr() {
         delete ptr_;
@@ -52,9 +53,9 @@ public:
     }
 
     void reset(Type* uniq = nullptr) {
-        auto tmpPtr = ptr_;
-        ptr_ = uniq;
-        if (tmpPtr != nullptr) {
+        auto tmpPtr = ptr_;        
+        ptr_ = uniq;     
+        if(tmpPtr != nullptr) {
             delete tmpPtr;
             tmpPtr = nullptr;
         }
