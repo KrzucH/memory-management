a9e4fd77f4bcf02b43b1480a3057f5cb1312a6a3
KrzucH <krzynowak93@gmail.com>
2024-07-05 15:24:20 +0200

    Change file name

---
+++ homework/unique_ptr/unique_ptr_tests.cpp
@@ -0,0 +1,67 @@
+#include "unique_ptr.hpp"
+#include <gtest/gtest.h>
+#include <memory>
+
+struct uniqAddTest {
+    uniqAddTest(int value1, int value2)
+        : value1_(value1),
+          value2_(value2) {}
+    int checkAddTest() {
+        return value1_ + value2_;
+    }
+
+private:
+    int value1_{};
+    int value2_{};
+};
+
+TEST(uniqAddTest, ArrowTest) {
+    uniq_ptr<uniqAddTest> uniq = new uniqAddTest(20, 30);
+    auto result = 50;
+
+    EXPECT_EQ(uniq->checkAddTest(), result);
+}
+
+TEST(int, DereferenceTest) {
+    uniq_ptr<int> uniq = new int(100);
+    auto result = 100;
+
+    EXPECT_EQ(*uniq, result);
+}
+
+TEST(uniqIntTest, MoveTest) {
+    uniq_ptr<uniqAddTest> uniq = new uniqAddTest(100, 100);
+    auto uniq1{std::move(uniq)};
+
+    EXPECT_EQ(uniq.get(), nullptr);
+
+    uniq_ptr<uniqAddTest> uniq2{};
+
+    EXPECT_EQ(uniq2.get(), nullptr);
+
+    auto uniq3 = std::move(uniq1);
+
+    EXPECT_EQ(uniq1.get(), nullptr);
+}
+
+TEST(int, ResetTest) {
+    uniq_ptr<int> uniq = new int(8);
+    constexpr int newValue = 6;
+
+    uniq.reset(new int(newValue));
+
+    ASSERT_EQ(*uniq, newValue);
+
+    uniq.reset();
+
+    EXPECT_EQ(uniq.get(), nullptr);
+}
+
+TEST(uniqAddTest, ReleaseTest) {
+    uniq_ptr<uniqAddTest> uniq = new uniqAddTest(100, 100);
+
+    uniq_ptr<uniqAddTest> uniq2 = uniq.release();
+
+    EXPECT_EQ(uniq.get(), nullptr);
+    EXPECT_NE(uniq2.get(), nullptr);
+}
